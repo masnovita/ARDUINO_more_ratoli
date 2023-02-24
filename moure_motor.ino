cb7081769719e6cd05351ae27cd0263649125643
masnovita <masnovita@gmail.com>
2023-02-24 09:22:24 +0100

    Modificada alçada roda

--- moure_motor.ino
+++ moure_motor.ino
@@ -3,10 +3,11 @@ int motorPin = 3;
 void setup()
 {
     pinMode(motorPin, OUTPUT);
-        for (int i=1;i<10;i++){
-    int dutyCycle = 50; // Valor entre 0 y 255
-    analogWrite(motorPin, dutyCycle); // Enviem senyal PWM al motor
-    delay(100); // 
+    // Moviment inicial
+    for (int i=1;i<10;i++){
+      int dutyCycle = 50; // Valor entre 0 y 255
+      analogWrite(motorPin, dutyCycle); // Enviem senyal PWM al motor
+      delay(100); // 
     }
 }
 
@@ -14,10 +15,10 @@ void loop()
 {
   // Inici
 
-    int dutyCycle = 38; // Valor entre 0 y 255
+    int dutyCycle = 30; // Valor entre 0 y 255. Valors petits amb poc temps no mouen el motor
     analogWrite(motorPin, dutyCycle); // Enviem senyal PWM al motor
-    delay(40); // Esperamos abans de parar el motor 
+    delay(50); // Esperamos abans de parar el motor 
     dutyCycle = 0; // Valor entre 0 y 255
     analogWrite(motorPin, dutyCycle); // Enviem senyal PWM al motor
-    delay(50000); // Esperem 50 segons
+    delay(30000); // Esperem 30 segons
 }
