d1d9c5e2780eb6dba2d7ce517fb09060329429e7
masnovita <masnovita@gmail.com>
2023-03-03 13:10:37 +0100

    Canvi temps (mes temps) i fotos actualitzades)

--- moure_motor.ino
+++ moure_motor.ino
@@ -15,10 +15,24 @@ void loop()
 {
   // Inici
 
+    //Si posem molt temps d'espera, el ratolí no es mou.
+    // Primer enviem un moviment i esperem 300 ms, enviem moviment i fem la pausa mes llarga.
+    
+    //Despertem ratolí 
     int dutyCycle = 30; // Valor entre 0 y 255. Valors petits amb poc temps no mouen el motor
+    analogWrite(motorPin, dutyCycle); // Enviem senyal PWM al motor
+    delay(40); // Esperamos abans de parar el motor 
+    dutyCycle = 0; // Valor entre 0 y 255
+    analogWrite(motorPin, dutyCycle); // Enviem senyal PWM al motor
+    // Esperem 500ms
+    delay(300);
+    // Fi despertem ratoli
+
+    // Enviem moviment
+    dutyCycle = 30; // Valor entre 0 y 255. Valors petits amb poc temps no mouen el motor
     analogWrite(motorPin, dutyCycle); // Enviem senyal PWM al motor
     delay(50); // Esperamos abans de parar el motor 
     dutyCycle = 0; // Valor entre 0 y 255
     analogWrite(motorPin, dutyCycle); // Enviem senyal PWM al motor
-    delay(20000); // Esperem 20 segons
+    delay(150000); // Esperem 150 segons
 }
